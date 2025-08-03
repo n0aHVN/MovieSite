43907666cfaa791597f4883a3b85a13d82af0698
n0aHVN <superherodung123@gmail.com>
2025-08-03 22:48:01 +0700

    Create Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"; // Changed from "react-router"
+import logoPath from "../../public/logo.png";
+import HeaderNavbar from "./Home/Navbar/HeaderNavbar";
+export default function Header() {
+    return (
+        <header className="bg-gray-900 text-white p-4">
+            <div className="container mx-auto flex flex-row items-center justify-between">
+                {/* Logo Section */}
+                <Link to="/" className="flex space-x-2 items-center justify-between">
+                    <img className="w-8 invert" src={logoPath} alt="Logo" />
+                    <p><strong>MovieSite</strong></p>
+                </Link>
+                {/* Navigation Section */}
+                <HeaderNavbar/>
+
+            </div>
+        </header>
+    );
+}
