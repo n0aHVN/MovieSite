a890e1d5c6714507e03009917ca0c239118fdda5
n0aHVN <superherodung123@gmail.com>
2025-08-04 19:57:41 +0700

    Update fetchPopularMovie.tsx

--- src/utils/fetchPopularMovie.tsx
+++ src/utils/fetchPopularMovie.tsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export async function fetchPopularMovies() {
+  const API_KEY = import.meta.env.VITE_API_KEY;
   const response = await axios.get(
-    "https://api.themoviedb.org/3/movie/popular?api_key=0048a7854ed872ee25fad62b1d7df6c1"
+    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
   );
   if (response.status !== 200) {
     throw new Error("Failed to fetch popular movies");
