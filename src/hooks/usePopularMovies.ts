f4b1c550380fbd4f5462576014a90517f406119a
n0aHVN <superherodung123@gmail.com>
2025-08-05 08:49:30 +0700

    Delete usePopularMovies.ts

--- src/hooks/usePopularMovies.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { fetchPopularMovies } from "../utils/fetchPopularMovie";
-
-export function usePopularMovies(){
-    return useQuery({
-        queryKey: ['popular-movies'],
-        queryFn: fetchPopularMovies
-    });
-}
