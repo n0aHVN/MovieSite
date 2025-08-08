792aa08af6a3b77c8c86de330740cbb115226e20
n0aHVN <superherodung123@gmail.com>
2025-08-08 22:25:57 +0700

    Update fetchMovieDB.tsx

--- src/utils/fetchMovieDB.tsx
+++ src/utils/fetchMovieDB.tsx
@@ -2,7 +2,11 @@ import axios from "axios";
 
 export async function fetchMovieDB<T>(url: string): Promise<T> {
   const API_KEY = import.meta.env.VITE_API_KEY;
-  const response = await axios.get(`${url}?api_key=${API_KEY}`);
+  const response = await axios.get(`${url}`,{
+    params: {
+      api_key: API_KEY,
+    }
+  });
   if (response.status !== 200) {
     throw new Error("Failed to fetch TheMovieDB");
   }
