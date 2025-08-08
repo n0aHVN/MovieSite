827a24b897660526346bb865e9125f5e1f4fbab1
n0aHVN <superherodung123@gmail.com>
2025-08-08 22:25:37 +0700

    Create useTopRatedTVSeries.ts

---
+++ src/hooks/useTopRatedTVSeries.ts
@@ -0,0 +1,10 @@
+
+import { useQuery } from "@tanstack/react-query";
+import { fetchMovieDB } from "@/utils/fetchMovieDB";
+
+export function useTopRatedTVSeries(){
+    return useQuery<any>({
+        queryKey: ['top-rated-tv-shows'],
+        queryFn: () => fetchMovieDB("https://api.themoviedb.org/3/tv/top_rated")
+    });
+}
