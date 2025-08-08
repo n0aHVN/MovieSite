3509604c5ddc6888a64e1bb5618bd3aefb824cec
n0aHVN <superherodung123@gmail.com>
2025-08-08 22:25:46 +0700

    Update AppLayout.tsx

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -3,7 +3,7 @@ import Header from '../components/Header';
 import Footer from '../components/Footer';
 export default function AppLayout() {
   return (
-    <div className="flex flex-col min-h-screen bg-black text-white">
+    <div className="flex flex-col min-h-screen bg-black-main text-white">
       <Header />
       <main className="flex-auto w-full h-full">
         <Outlet/>
