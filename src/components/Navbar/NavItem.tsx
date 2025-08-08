bed6a001afddb28ff03a028837a625ecaacc8551
n0aHVN <superherodung123@gmail.com>
2025-08-08 22:25:20 +0700

    Update NavItem.tsx

--- src/components/Navbar/NavItem.tsx
+++ src/components/Navbar/NavItem.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, type ReactNode } from "react";
+import { useContext, type ReactNode } from "react";
 import { Link } from "react-router-dom";
 import { NavContext } from "./Navbar";
 
